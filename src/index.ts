c5a5d9bc8e6a0cbdfacc544c07a7b58dff9f5692
Akhil <amdlinux13@gmail.com>
2023-10-03 00:01:06 +0530

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello Typescripts")
+import express from "express";
+import http from "http";
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express()
+
+app.use(cors({
+    credentials:true
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+//instantiating a server
+
+const server = http.createServer()
